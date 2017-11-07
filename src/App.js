e3eb22b8b9650ce4ab27094f94074aa4056057f8
Mariley20 <mariley2018@gmail.com>
2017-11-07 09:48:55 -0500

    create actions and store

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from "redux-zero/react";
+import { addComment, removeComment, reportAbusive } from "./actions";
+import './App.css';
+const App = ({ commentary, selectedComment }) => {
+return (
+    <div>hola</div>
+)
+}
+
+const mapToProps = ({ commentary, selectedComment }) => ({ commentary, selectedComment });
+
+export default connect(mapToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-    HashRouter,
-    Route,
-    Switch
-} from 'react-router-dom';
-
-import indexRoutes from './routes/index.jsx';
-
+import { Provider } from 'redux-zero/react';
+import store from "./store";
+import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import './assets/css/bootstrap.min.css';
-import './assets/sass/light-bootstrap-dashboard.css';
-import './assets/css/demo.css';
-import './assets/css/pe-icon-7-stroke.css';
-
-ReactDOM.render((
-    <HashRouter>
-        <Switch>
-            {
-                indexRoutes.map((prop,key) => {
-                    return (
-                        <Route path={prop.path} component={prop.component}  key={key}/>
-                    );
-                })
-            }
-        </Switch>
-    </HashRouter>
-), document.getElementById('root'));
+const Index = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+ReactDOM.render(<Index />, document.getElementById('root'));
 registerServiceWorker();
