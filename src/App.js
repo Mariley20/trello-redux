c54a3bc1ad1f812ba77001d94a6b9039da3c5c07
Mariley20 <mariley2018@gmail.com>
2017-11-07 10:45:04 -0500

    signIn

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import { connect } from "redux-zero/react";
 import { } from "./actions";
 import './App.css';
+import {SignIn} from './SignIn';
 const App = ({ commentary, selectedComment }) => {
 return (
-    <div>hola</div>
+    <div className="container">
+        <Header />
+        <SignIn />
+    </div>
 )
 }
-
+const Header = () => {
+    return (
+        <div>
+            <img className="logo" src="https://phoenix-trello.herokuapp.com/images/logo-11ecccd65d1c7977997eb6f0bc0002ad.png?vsn=d" />
+        </div>
+    )
+}
 const mapToProps = ({ commentary, selectedComment }) => ({ commentary, selectedComment });
 
 export default connect(mapToProps)(App);
---
+++ src/SignIn.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Button, Form, FormControl, FormGroup, Col, Checkbox } from 'react-bootstrap';
+
+export const SignIn =() => {
+    return(
+        <div>
+            <Form horizontal>
+    <FormGroup controlId="formHorizontalEmail">
+      <Col sm={10}>
+        <FormControl type="email" placeholder="Email" />
+      </Col>
+    </FormGroup>
+
+    <FormGroup controlId="formHorizontalPassword">
+      <Col sm={10}>
+        <FormControl type="password" placeholder="Password" />
+      </Col>
+    </FormGroup>
+
+    <FormGroup>
+      <Col smOffset={2} sm={10}>
+        <Checkbox>Remember me</Checkbox>
+      </Col>
+    </FormGroup>
+
+    <FormGroup>
+      <Col smOffset={2} sm={10}>
+        <Button type="submit">
+          Sign in
+        </Button>
+      </Col>
+    </FormGroup>
+  </Form>
+        </div>
+    )
+}
